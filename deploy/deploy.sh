d85786b7cc5dfd1c08ede32b9b35014e1eb1693f
badraAli <ardabali.b@gmail.com>
2025-02-14 14:58:04 +0000

    last changed made on 14/02/25 14:57

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -28,13 +28,13 @@ echo "Copying files to EC2..."
 scp -i deploy_key.pem \
     -o StrictHostKeyChecking=no \
     app.tar \
-    ec2-user@${EC2_HOST}:~/
+    ubuntu@${EC2_HOST}:~/
 
 # Deploy on EC2
 echo "Deploying on EC2..."
 ssh -i deploy_key.pem \
     -o StrictHostKeyChecking=no \
-    ec2-user@${EC2_HOST} \
+    ubuntu@${EC2_HOST} \
     "docker load < app.tar && \
      docker stop $APP_NAME || true && \
      docker rm $APP_NAME || true && \
